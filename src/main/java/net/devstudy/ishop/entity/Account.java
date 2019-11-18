b02858caa9d7819b30a7ba3acff114e2b98f1cd9
Anuarbek <anuarbekzhamanbaev@gmail.com>
2019-11-18 16:22:09 +0600

    Создание сущностей

---
+++ src/main/java/net/devstudy/ishop/entity/Account.java
@@ -0,0 +1,32 @@
+package net.devstudy.ishop.entity;
+
+public class Account extends AbstractEntity<Integer> {
+
+    private String name;
+    private String email;
+
+    public Account() {
+        super();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Account [id=%s, name=%s, email=%s]", getId(), name, email);
+    }
+}
